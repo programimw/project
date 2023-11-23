a0d918d2de073b9858988325bac03e17710fefc4
Brunilda Muco <brunildamuco6@gmail.com>
2023-11-23 13:38:14 +0100

    Shtimi i hashit te passwordit ne databaze. Validimi i emailit qe te jete unik

--- backend.php
+++ backend.php
@@ -1,7 +1,4 @@
 <?php
-//error_reporting(0);
-//session_start();
-//print_r($_POST);
 require_once "includes/connect.php";
 
 // Signup User
@@ -12,9 +9,10 @@ if (isset($_POST['action']) && $_POST['action'] == "signup"){
     $name = mysqli_real_escape_string($conn, $_POST['name']);
     $surname = mysqli_real_escape_string($conn, $_POST['surname']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
     $confirmPassword = mysqli_real_escape_string($conn, $_POST['confirmPassword']);
     $alphanumericRegex = '/^[a-zA-Z0-9]+$/';
+    $password_hashed = mysqli_real_escape_string($conn, password_hash($_POST['password'], PASSWORD_BCRYPT));
+
     /*
      * Validimi i te dhenave
      * */
@@ -29,12 +27,46 @@ if (isset($_POST['action']) && $_POST['action'] == "signup"){
       exit;
     }
 
+
+    /**
+     * Validimi i E-Mailit. Shohim nese E-Maili egziston
+     */
+    $query_check = "SELECT id 
+                    FROM users 
+                    WHERE email = '".$email."'";
+
+    $result_check = mysqli_query($conn,$query_check);
+
+    if (!$result_check){
+        echo json_encode(
+            array(
+                "status"=>201,
+                "message"=>"Internal Server Error ".__LINE__,
+                "error" => mysqli_error($conn)
+            ));
+        exit;
+    }
+
+    if (mysqli_num_rows($result_check)){
+        echo json_encode(
+            array(
+                "status"=>201,
+                "message"=>"User with email: ".$email." aready exists on system.",
+                "tag" =>"emailHelp",
+                "error" => mysqli_error($conn)
+            ));
+        exit;
+    }
+
+
     /**
      * Shtimi i te dhenave ne databaze
      */
     $query_insert = "INSERT INTO users
                      set name    = '".$name."',
                      surname    = '".$surname."',
+                     password    = '".$password_hashed."',
+                     created_at    = '".date("Y-m-d")."',
                      email    = '".$email."'";
 
     $result_insert = mysqli_query($conn, $query_insert);
